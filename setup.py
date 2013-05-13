7f829d68d618678c0c99dd3574d93a892a257b27
Efremov Alexey <lexich121@gmail.com>
2013-05-13 14:23:38 +0400

    update manifest setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 #-*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ckeditor',
-    version='0.0.1',
+    version='0.0.3',
+    packages=['ckeditor'],
     include_package_data=True,
-    packages=find_packages(),
+    license='BSD License',
+    description='CKEDITOR',
+    long_description=README,
     url='https://github.com/lexich/django-ckeditor',
-    license='BSD',
     author='lexich',
     author_email='lexich121@gmail.com',
-    description='django admin interface for GenericForeignKey fields',
-    long_description = open("README.md","r").read()
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
